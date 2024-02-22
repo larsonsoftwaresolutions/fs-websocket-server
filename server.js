3bc7970eefd89a9519a18cf44cf622ea97414d53
segwitnitwit <segwitnitwit@gmail.com>
2024-02-22 01:01:33 -0600

    test telegram bot

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ import {
 import "dotenv/config";
 import { AUTOGLYPH_SERIES } from "./autoglyphSeries.js";
 import bodyParser from "body-parser";
+import TelegramBot from "node-telegram-bot-api";
 
 let allAddresses = [];
 let allContacts = [];
@@ -26,11 +27,11 @@ const jsonParser = bodyParser.json();
 
 // USE FOR MAINNET
 const graphqlEndpoint = "https://fountaindigital.xyz/graphql";
-const alchemyBaseURL = `https://eth-mainnet.g.alchemy.com/nft/v3/${process.env.ALCHEMY_API_KEY}`;
+// const alchemyBaseURL = `https://eth-mainnet.g.alchemy.com/nft/v3/${process.env.ALCHEMY_API_KEY}`;
 
 // USE FOR TESTNET
 // const graphqlEndpoint = "http://localhost:3000/graphql";
-// const alchemyBaseURL = `https://eth-sepolia.g.alchemy.com/nft/v3/${process.env.ALCHEMY_API_KEY}`;
+const alchemyBaseURL = `https://eth-sepolia.g.alchemy.com/nft/v3/${process.env.ALCHEMY_API_KEY}`;
 
 const alchemy_options = {
   method: "GET",
@@ -39,6 +40,10 @@ const alchemy_options = {
 
 const app = express();
 const port = process.env.PORT || 3005;
+// to get channel_id go to: https://api.telegram.org/botXXX:YYYY/getUpdates
+const telegramBot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN, {
+  polling: true,
+});
 
 app.get("/test", (req, res) => {
   res.status(200).send("Nice: " + reservoirConnected + " " + transfersCreated);
@@ -69,15 +74,15 @@ async function sendGraphQL(_query, _variables) {
 }
 
 // USE FOR MAINNET
-const wss = new WebSocket(
-  `wss://ws.reservoir.tools?api_key=${process.env.WEBSOCKET_API_KEY}`
-);
+// const wss = new WebSocket(
+//   `wss://ws.reservoir.tools?api_key=${process.env.WEBSOCKET_API_KEY}`
+// );
 
 // USE FOR TESTNET
 // console.log("WEBSOCKET API EKY: " + process.env.WEBSOCKET_API_KEY)
-// const wss = new WebSocket(
-//   `wss://ws-sepolia.reservoir.tools?api_key=${process.env.WEBSOCKET_API_KEY}`
-// );
+const wss = new WebSocket(
+  `wss://ws-sepolia.reservoir.tools?api_key=${process.env.WEBSOCKET_API_KEY}`
+);
 
 // Schedule updates every 5 minutes (adjust the interval as needed).
 // setInterval(schedulePeriodicUpdates, 5 * 60 * 1000);
@@ -194,6 +199,11 @@ wss.on("open", function open() {
               console.log(
                 "ASSET_MOVED_OUT -- TRANSFER RESPONSE: " +
                   JSON.stringify(transferResponse)
+              );
+
+              telegramBot.sendMessage(
+                process.env.TELEGRAM_CHAT_ID,
+                JSON.stringify(transferResponse)
               );
             } catch (error) {
               console.error(error);
@@ -388,10 +398,10 @@ wss.on("open", function open() {
                     ercType: nft.tokenType,
                     ownerAddress: toContact.address,
                     contractAddress: nft.contract.address.toLowerCase(),
-                    slug: nft.collection.slug,
-                    // slug: "test-collection",
-                    collectionName: nft.collection.name,
-                    // collectionName: "Test Collection",
+                    // slug: nft.collection.slug,
+                    slug: "test-collection",
+                    // collectionName: nft.collection.name,
+                    collectionName: "Test Collection",
                     contact: toContact.contact._id,
                     contactName: toContact.contact.name,
                     quantity: Number(transferData.amount),
@@ -590,10 +600,10 @@ wss.on("open", function open() {
                     ercType: nft.tokenType,
                     ownerAddress: toContact.address,
                     contractAddress: nft.contract.address.toLowerCase(),
-                    slug: nft.collection.slug,
-                    // slug: "test-collection",
-                    collectionName: nft.collection.name,
-                    // collectionName: "Test Collection",
+                    // slug: nft.collection.slug,
+                    slug: "test-collection",
+                    // collectionName: nft.collection.name,
+                    collectionName: "Test Collection",
                     contact: toContact.contact._id,
                     contactName: toContact.contact.name,
                     quantity: Number(transferData.amount),
@@ -735,10 +745,10 @@ wss.on("open", function open() {
                 ercType: nft.tokenType,
                 ownerAddress: toContact.address,
                 contractAddress: nft.contract.address.toLowerCase(),
-                slug: nft.collection.slug,
-                // slug: "test-collection",
-                collectionName: nft.collection.name,
-                // collectionName: "Test Collection",
+                // slug: nft.collection.slug,
+                slug: "test-collection",
+                // collectionName: nft.collection.name,
+                collectionName: "Test Collection",
                 contact: toContact.contact._id,
                 contactName: toContact.contact.name,
                 quantity: Number(transferData.amount),
@@ -762,8 +772,8 @@ wss.on("open", function open() {
                 from: {
                   address: from,
                 },
-                collectionName: nft.collection.name,
-                // collectionName: "Test Collection",
+                // collectionName: nft.collection.name,
+                collectionName: "Test Collection",
                 contractAddress: token.contract,
                 tokenId: Number(token.tokenId),
               },
@@ -778,7 +788,13 @@ wss.on("open", function open() {
               transferVariables
             );
             console.log(
-              "TRANSFER RESPONSE: " + JSON.stringify(transferResponse)
+              "ASSET_MOVED_IN -- TRANSFER RESPONSE: " +
+                JSON.stringify(transferResponse)
+            );
+
+            telegramBot.sendMessage(
+              process.env.TELEGRAM_CHAT_ID,
+              JSON.stringify(transferResponse)
             );
           } catch (error) {
             console.error(error);
