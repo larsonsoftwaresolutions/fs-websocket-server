52098c2d6b81542531e41a2b79fd08aa33d1cd40
segwitnitwit <segwitnitwit@gmail.com>
2024-02-21 23:41:02 -0600

    switch over from sepolia

--- server.js
+++ server.js
@@ -26,11 +26,11 @@ const jsonParser = bodyParser.json();
 
 // USE FOR MAINNET
 const graphqlEndpoint = "https://fountaindigital.xyz/graphql";
-// const alchemyBaseURL = `https://eth-mainnet.g.alchemy.com/nft/v3/${process.env.ALCHEMY_API_KEY}`;
+const alchemyBaseURL = `https://eth-mainnet.g.alchemy.com/nft/v3/${process.env.ALCHEMY_API_KEY}`;
 
 // USE FOR TESTNET
 // const graphqlEndpoint = "http://localhost:3000/graphql";
-const alchemyBaseURL = `https://eth-sepolia.g.alchemy.com/nft/v3/${process.env.ALCHEMY_API_KEY}`;
+// const alchemyBaseURL = `https://eth-sepolia.g.alchemy.com/nft/v3/${process.env.ALCHEMY_API_KEY}`;
 
 const alchemy_options = {
   method: "GET",
@@ -69,15 +69,15 @@ async function sendGraphQL(_query, _variables) {
 }
 
 // USE FOR MAINNET
-// const wss = new WebSocket(
-//   `wss://ws.reservoir.tools?api_key=${process.env.WEBSOCKET_API_KEY}`
-// );
+const wss = new WebSocket(
+  `wss://ws.reservoir.tools?api_key=${process.env.WEBSOCKET_API_KEY}`
+);
 
 // USE FOR TESTNET
 // console.log("WEBSOCKET API EKY: " + process.env.WEBSOCKET_API_KEY)
-const wss = new WebSocket(
-  `wss://ws-sepolia.reservoir.tools?api_key=${process.env.WEBSOCKET_API_KEY}`
-);
+// const wss = new WebSocket(
+//   `wss://ws-sepolia.reservoir.tools?api_key=${process.env.WEBSOCKET_API_KEY}`
+// );
 
 // Schedule updates every 5 minutes (adjust the interval as needed).
 // setInterval(schedulePeriodicUpdates, 5 * 60 * 1000);
