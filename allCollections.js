7a6b0e28a4810b3d34e0777b392f3ffb067abee3
segwitnitwit <segwitnitwit@gmail.com>
2024-02-22 15:51:32 -0600

    update collections / alphabetize

--- allCollections.js
+++ allCollections.js
@@ -1,50 +1,49 @@
 const allCollections = [
+    "0xa7d8d9ef8d8ce8992df33d8b8cf4aebabd5bd270", // artblocks
     '0xd4e4078ca3495de5b1d4db434bebc5a986197782', // Autoglyphs ==============================================> DONE
     '0xed5af388653567af2f388e6224dc7c4b3241c544', // Azuki ===================================================> DONE
-    '0xb6a37b5d14d502c3ab0ae6f3a0e058bc9517786e', // Azuki Elementals ========================================> DONE
-    '0xd92e44ac213b9ebda0178e1523cc0ce177b7fa96', // Beeple: The Everydays  ==================================> DONE
+    '0x44541d1ff5e8a1ed22566df771995fff9139f122', // Beeple: Everydays
+    '0xd92e44ac213b9ebda0178e1523cc0ce177b7fa96', // Beeple: Everydays - The 2020 collection  ==================================> DONE
+    '0x12f28e2106ce8fd8464885b80ea865e98b465149', // Beeple: Genesis Collection
+    '0xdd012153e008346591153fff28b0dd6724f0c256', // Beeple: Spring/Summer Collection 2021
+    '0x30b2e98609d66dcc93cb47a095809d03188ea83d', // Beeple: Uncertain Future
     '0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d', // Bored Apes ==============================================> DONE
+    '0xdfde78d2baec499fe18f2be74b6c287eed9511d7', // BrainDrops
+    "0xd90829c6c6012e4dde506bd95d7499a04b9a56de", // broken keys
     '0xd3414bd4c0c60e8da5e432e8fe8f97cb12ef9874', // Checks - Twelve Checks ==================================> DONE
     '0x34eebee6942d8def3c125458d1a86e0a897fd6f9', // Checks - VV Edition
     '0x379b5616a6afe6bc6baa490ef8fd98bf6d7db45c', // Checks - VV Elements
     '0x036721e5a769cc48b3189efbb9cce4471e8a48b1', // Checks - VV Originals
+    "0x1cb1a5e65610aeff2551a50f76a87a7d3fb649c6", // CrypToadz
     '0x42069abfe407c60cf4ae4112bedead391dba1cdb', // CryptoDickButts
     '0xb47e3cd837ddf8e4c57f05d70ab865de6e193bbb', // CryptoPunks =============================================> DONE
+    "0x000000000000003607fce1ac9e043a86675c5c2f", // cryptopunks721 (new wrapped contaract) ==================> DONE
     '0x73da73ef3a6982109c4d5bdb0db9dd3e3783f313', // Curio Cards
     '0xd754937672300ae6708a51229112de4017810934', // Deafbeef: Series 0-5 
-    '0xdfde78d2baec499fe18f2be74b6c287eed9511d7', // BrainDrops
-    '0xc143bbfcdbdbed6d454803804752a064a622c1f3', // Grifters
-    '0xe012baf811cf9c05c408e879c399960d1f305903', // Kodas
-    '0x7bd29408f11d2bfc23c34f18275bbf23bb716bc7', // Meebits
-    '0x60e4d786628fea6478f785a6d7e704777c86a7c6', // Mutant Apes
-    '0x9c8ff314c9bc7f6e59a9d9225fb22946427edc03', // Nouns
-    '0x845dd2a7ee2a92a0518ab2135365ed63fdba0c88', // QQL
-    '0xc73b17179bf0c59cd5860bb25247d1d1092c1088', // QQL Mint Pass
-    '0xed5af388653567af2f388e6224dc7c4b3241c544', // Skulls of Luci
-    '0x41a322b28d0ff354040e2cbc676f0320d8c8850d', // SuperRare - The Lost Robbies ============================> DONE
-    '0xb932a70a57673d89f4acffbe830e8ed7f75fb9e0', // SuperRare - Other stuff? ================================> DONE
     '0x82c7a8f707110f5fbb16184a5933e9f78a34c6ab', // Emblem Vault - The Nakamoto / RAREPEPE
-    '0x7e6027a6a84fc1f6db6782c523efe62c923e46ff', // Rare Pepe Curated - The Nakamoto / RAREPE
+    '0xc143bbfcdbdbed6d454803804752a064a622c1f3', // Grifters
+    "0x46ac8540d698167fcbb9e846511beb8cf8af9bd8", // Homegrown by Julie Blackmon
     "0x495f947276749ce646f68ac8c248420045cb7b5e", // Justin Aversano's Twin Flames
     "0xe6501d00ddca2ab22c655c612e73ed822d9256a2", // Justin Aversano - Smoke and Mirrors - Collection of 78 Tarot Portraits
     "0x495f947276749ce646f68ac8c248420045cb7b5e", // Justin Aversano - Smoke and Mirrors - Physical Tarot Scrolls
-    "0x46ac8540d698167fcbb9e846511beb8cf8af9bd8", // Homegrown by Julie Blackmon
-    "0x6b00de202e3cd03c523ca05d8b47231dbdd9142b", // Tom Sachs: Rocket Factory - Rockets
-    "0x11595ffb2d3612d810612e34bc1c2e6d6de55d26", // Tom Sachs: Rocket Factory - Components
-    "0xaadc2d4261199ce24a4b0a57370c4fcf43bb60aa", // The Currency - Damien Hirst
-    "0x1cb1a5e65610aeff2551a50f76a87a7d3fb649c6", // CrypToadz
-    "0x31385d3520bced94f77aae104b406994d8f2168c", // bgans V2 ================================================> DONE
-    "0x9126b817ccca682beaa9f4eae734948ee1166af1", // bgans V1 =================================================> DONE
-    "0xa7d8d9ef8d8ce8992df33d8b8cf4aebabd5bd270", // artblocks
-    "0x11de4a45d9dffe96b30cba3dde25f97f6db26c0b", // yellow flappy
+    '0xe012baf811cf9c05c408e879c399960d1f305903', // Kodas
+    '0x7bd29408f11d2bfc23c34f18275bbf23bb716bc7', // Meebits
     "0xc3f733ca98e0dad0386979eb96fb1722a1a05e69", // mooncats
-    "0x5ab44d97b0504ed90b8c5b8a325aa61376703c88", // e30d
-    "0x72a94e6c51cb06453b84c049ce1e1312f7c05e2c", // wiiides
+    '0x60e4d786628fea6478f785a6d7e704777c86a7c6', // Mutant Apes
+    '0x9c8ff314c9bc7f6e59a9d9225fb22946427edc03', // Nouns
+    '0x845dd2a7ee2a92a0518ab2135365ed63fdba0c88', // QQL
+    '0x7e6027a6a84fc1f6db6782c523efe62c923e46ff', // Rare Pepe Curated - The Nakamoto / RAREPE
+    "0xbbdc68d42b011aab7d987ccb488047c4aba3c7e1", // sam spratt - studies
+    '0xed5af388653567af2f388e6224dc7c4b3241c544', // Skulls of Luci - sam spratt
+    '0x41a322b28d0ff354040e2cbc676f0320d8c8850d', // SuperRare - The Lost Robbies ============================> DONE
+    '0xb932a70a57673d89f4acffbe830e8ed7f75fb9e0', // SuperRare - Other stuff? ================================> DONE
+    "0xaadc2d4261199ce24a4b0a57370c4fcf43bb60aa", // The Currency - Damien Hirst
+    "0x6b00de202e3cd03c523ca05d8b47231dbdd9142b", // Tom Sachs: Rocket Factory - Rockets
+    "0x11595ffb2d3612d810612e34bc1c2e6d6de55d26", // Tom Sachs: Rocket Factory - Components    
     "0xb7f7f6c52f2e2fdb1963eab30438024864c313f6", // wrapped cryptopunks =====================================> DONE (no traits)
-    "0x000000000000003607fce1ac9e043a86675c5c2f", // cryptopunks721 (new wrapped contaract) ==================> DONE
-    "0x299e78808918f3f4fa4d1b97b0706fae70dfb83a", // BGAN Pillars - testing purposes
-    "0xfc4a9d4cd18dea2acd38d8a4f292dfa5006ec838", // sepolia nonsense - TLBB
-    "0xdcd8dbb5624e2a44f9b3882f56b62d2d1920f660", // some random sepolia collection - SBK
+    // "0x299e78808918f3f4fa4d1b97b0706fae70dfb83a", // BGAN Pillars - testing purposes
+    // "0xfc4a9d4cd18dea2acd38d8a4f292dfa5006ec838", // sepolia nonsense - TLBB
+    // "0xdcd8dbb5624e2a44f9b3882f56b62d2d1920f660", // some random sepolia collection - SBK
 ]
 
 export {allCollections}
